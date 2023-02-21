8560ba6145477e72b8cacc0f678afe14b0c30114
Sadie Amato <saamato@email.unc.edu>
2023-02-21 17:53:22 -0500

    prelim work on logic of files

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -1,1 +1,46 @@
 #!/usr/bin/env node
+import { rps } from "../lib/rpsls.js"
+
+
+// ---------- Requirements -----------
+// Two versions of the game (RPS and RPSLS) in one package.
+// If the command or function is called without an argument, it should return only the shot for one player, e.g. {"player":"rock"}.
+// If the command or function is called with an argument, it should return the results of a game between a player and an opponent, e.g. {"player":"rock","opponent":"scissors","result":"lose"}.
+// If you supply any argument other than those the individual function is expecting, the user should be presented with an error and a suggestion of the available options (e.g. "rock", "paper", "scissors" for RPS and "rock", "paper", "scissors", "lizard", "spock" for RPSLS).
+// Each command should echo its own help text with usage examples when invoked with -h or --help (see below).
+// Each command should echo rules when invoked with -r --rules (see below).
+// If an out-of-range argument is supplied to your functions, the function should return an error on console.error() indicating that [ARGUMENT] is out of range.
+
+
+// Process args and put them into 'args'
+let args = minimist(process.argv.slice(2));
+
+
+// If given the -h or -help option in command line, show help msg and exit 0
+if (args.h || args.help) {
+    console.log('Usage: node-rps [SHOT]\nPlay Rock Paper Scissors (RPS)\n\t-h, --help      display this help message and exit\n\t-r, --rules     display the rules and exit\n\nExamples:\n\tnode-rps        Return JSON with single player RPS result.\n\t\t\t\te.g. {"player":"rock"}\n\tnode-rps rock   Return JSON with results for RPS played against a simulated opponent.\n\t\t\t\te.g {"player":"rock","opponent":"scissors","result":"win"}');
+    process.exit(0);
+}
+
+// If given -r or -rules, show rules msg and exit 0
+if (args.r || args.rules) {
+    console.log('Rules for Rock Paper Scissors:\n\n\t- Scissors CUTS Paper\n\t- Paper COVERS Rock\n\t- Rock CRUSHES Scissors');
+    process.exit(0);
+}
+
+// Set shot equal to unlabeled argument
+let shot = args._[0];
+
+try {
+	console.log(JSON.stringify(rps(shot)));
+} catch (e) {
+    // If error happened in rps, log rules and help messages
+    console.log('Usage: node-rps [SHOT]\nPlay Rock Paper Scissors (RPS)\n\t-h, --help      display this help message and exit\n\t-r, --rules     display the rules and exit\n\nExamples:\n\tnode-rps        Return JSON with single player RPS result.\n\t\t\t\te.g. {"player":"rock"}\n\tnode-rps rock   Return JSON with results for RPS played against a simulated opponent.\n\t\t\t\te.g {"player":"rock","opponent":"scissors","result":"win"}');
+    console.log('Rules for Rock Paper Scissors:\n\n\t- Scissors CUTS Paper\n\t- Paper COVERS Rock\n\t- Rock CRUSHES Scissors');
+    process.exit(0);
+}
+
+
+
+
+
--- bin/rpsls-cli.js
+++ bin/rpsls-cli.js
@@ -1,1 +1,56 @@
 #!/usr/bin/env node
+import { rpsls } from "../lib/rpsls.js"
+
+
+let help = `Usage: node-rpsls [SHOT]
+Play the Lizard-Spock Expansion of Rock Paper Scissors (RPSLS)!
+
+  -h, --help        display this help message and exit
+  -r, --rules       display the rules and exit
+
+Examples:
+  node-rpsls        Return JSON with single player RPSLS result.
+                    e.g. {"player":"rock"}
+  node-rpsls rock   Return JSON with results for RPSLS played against a simulated opponent.
+                    e.g {"player":"rock","opponent":"Spock","result":"lose"}`
+
+let rules = `Rules for the Lizard-Spock Espansion of Rock Paper Scissors:
+
+- Scissors CUTS Paper
+- Paper COVERS Rock
+- Rock SMOOSHES Lizard
+- Lizard POISONS Spock
+- Spock SMASHES Scissors
+- Scissors DECAPITATES Lizard
+- Lizard EATS Paper
+- Paper DISPROVES Spock
+- Spock VAPORIZES Rock
+- Rock CRUSHES Scissors`
+
+
+// Process args and put them into 'args'
+let args = minimist(process.argv.slice(2));
+
+// If given the -h or -help option in command line, show help msg and exit 0
+if (args.h || args.help) {
+    console.log(help);
+    process.exit(0);
+}
+
+// If given the -r or -rules option in command line, show rules msg and exit 0
+if (args.r || args.rules) {
+    console.log(rules);
+    process.exit(0);
+}
+
+// Set shot equal to unlabeled argument
+let shot = args._[0]
+
+try {
+	console.log(JSON.stringify(rpsls(shot)));
+} catch (e) {
+    // If error happened in rpsls, log rules and help messages
+    console.log(help);
+    console.log(rules);
+}
+
